e96e6378e9c40a106db5d3d6b89229af55bdcf72
dzwicker <dzwicker@TeamCity>
2020-06-25 12:38:47 +0000

    TeamCity change in 'eService / Backend / Inspections-Test' project: general settings of 'Inspections' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -35,7 +35,7 @@ project {
 object Inspections : BuildType({
     name = "Inspections"
 
-    artifactRules = "+:results-*.zip"
+    artifactRules = "+:results*.tar.gz"
 
     params {
         param("teamcity.dont.delete.temp.result.dir", "true")
